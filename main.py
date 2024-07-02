019042cefefc1cafd10b1ad19c4a77d26fd572f4
Lazaros Gogos <lazarosg@csd.auth.gr>
2024-07-03 01:18:31 +0300

    minor mistake, wrong model pretrained for another 500 epochs. fixed now. the output is kept for research purposes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import yaml
 
 from src.utils.distributed import init_distributed
 from src.train import main as app_main
-from src.test import main as test_app_main
+# from src.test import main as test_app_main
 
 parser = argparse.ArgumentParser()
 parser.add_argument(

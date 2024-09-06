88b6dde537ba0723d6fa6a17b488a222568c85e4
Lazaros Gogos <lazarosg@csd.auth.gr>
2024-09-06 23:08:39 +0300

    Added INTER image KL divergence calculation to PKT module for future use

--- src/PKT.py
+++ src/PKT.py
@@ -26,3 +26,41 @@ def cosine_similarity_loss(output_net, target_net, eps=0.0000001):
     loss = torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)))
 
     return loss
+
+
+# take inter-image similarity 
+def cosine_similarity_loss_inter(output_net, target_net, output_net_2, target_net_2, eps=0.0000001):
+    # Normalize each vector by its norm
+    output_net_norm = torch.sqrt(torch.sum(output_net ** 2, dim=1, keepdim=True))
+    output_net = output_net / (output_net_norm + eps)
+    output_net[output_net != output_net] = 0
+
+    target_net_norm = torch.sqrt(torch.sum(target_net ** 2, dim=1, keepdim=True))
+    target_net = target_net / (target_net_norm + eps)
+    target_net[target_net != target_net] = 0
+
+    # Normalize each vector by its norm a second time for another image
+    output_net_norm_2 = torch.sqrt(torch.sum(output_net_2 ** 2, dim=1, keepdim=True))
+    output_net_2 = output_net_2 / (output_net_norm_2 + eps)
+    output_net_2[output_net_2 != output_net_2] = 0
+
+    target_net_norm_2 = torch.sqrt(torch.sum(target_net_2 ** 2, dim=1, keepdim=True))
+    target_net_2 = target_net_2 / (target_net_norm_2 + eps)
+    target_net_2[target_net_2 != target_net_2] = 0
+
+    # Calculate the cosine similarity
+    model_similarity = torch.mm(output_net, output_net_2.transpose(0, 1))
+    target_similarity = torch.mm(target_net, target_net_2.transpose(0, 1))
+
+    # Scale cosine similarity to 0..1
+    model_similarity = (model_similarity + 1.0) / 2.0
+    target_similarity = (target_similarity + 1.0) / 2.0
+
+    # Transform them into probabilities
+    model_similarity = model_similarity / torch.sum(model_similarity, dim=1, keepdim=True)
+    target_similarity = target_similarity / torch.sum(target_similarity, dim=1, keepdim=True)
+
+    # Calculate the KL-divergence
+    loss = torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)))
+
+    return loss

00d18b19065c0ec281c6ed2947add2ea70e15b7b
Lazaros Gogos <lazarosg@csd.auth.gr>
2024-11-01 18:04:24 +0200

    updated train.py for no duplicate lines due to merge

--- src/train.py
+++ src/train.py
@@ -87,25 +87,19 @@ def evaluate_knn(encoder, train_loader, test_loader, device='cuda'):
     with torch.no_grad():
         for images, labels in train_loader:
             features = encoder(images.to(device)).mean(dim=1)  # Get embeddings
-            features = encoder(images.to(device)).mean(dim=1)  # Get embeddings
             train_features.append(features.cpu().numpy())
-            train_labels.append(np.array(labels))
             train_labels.append(np.array(labels))
             
         for images, labels in test_loader:
             features = encoder(images.to(device)).mean(dim=1)
-            features = encoder(images.to(device)).mean(dim=1)
             test_features.append(features.cpu().numpy())
-            test_labels.append(np.array(labels))
             test_labels.append(np.array(labels))
     
     train_features = np.concatenate(train_features)
     train_labels = np.concatenate(train_labels).astype(int)
-    train_labels = np.concatenate(train_labels).astype(int)
     test_features = np.concatenate(test_features)
     test_labels = np.concatenate(test_labels).astype(int)
 
-    test_labels = np.concatenate(test_labels).astype(int)
 
     # KNN classifier
     classifier = KNeighborsClassifier(n_neighbors=5)
@@ -294,19 +288,6 @@ def main(args, resume_preempt=False):
         collator=None,  # No mask collator for supervised data
         pin_mem=pin_mem,
         training=True,
-        num_workers=num_workers,
-        world_size=world_size,
-        rank=rank,
-        root_path=root_path,
-        image_folder=image_folder,
-        copy_data=copy_data,)
-    
-    _, test_loader, _ = make_imagenet1k_supervised(
-        transform=transform,
-        batch_size=batch_size,
-        collator=None,  # No mask collator for supervised data
-        pin_mem=pin_mem,
-        training=False,
         num_workers=num_workers,
         world_size=world_size,
         rank=rank,

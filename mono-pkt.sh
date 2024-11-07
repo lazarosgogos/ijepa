712706d9d8e0f8f47892d2bc48780c28acd6c081
Lazaros Gogos <lazarosg@csd.auth.gr>
2024-11-07 14:27:28 +0200

    done pretraining on IN100, updatinig outputs (model collapsed)

--- mono-pkt.sh
+++ mono-pkt.sh
@@ -2,13 +2,13 @@
 
 #SBATCH -J IJEPA_IN100_L2_PKT
 #SBATCH -t 6-00:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=256G
 #SBATCH -c 16
 #SBATCH -n 1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lazarosg@csd.auth.gr
 #SBATCH -p ampere
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --qos=ampere-extd
 
 module load gcc miniconda3 cuda
@@ -17,5 +17,5 @@ conda activate ijepa
 export PATH=$CONDA_PREFIX/bin:$PATH
 
 python main.py  \
-	--fname configs/in100_vitl16_ep300_pkt.yaml \
-	--devices cuda:0 cuda:1
+	--fname configs/in100_vitb14_ep600_pkt.yaml \
+	--devices cuda:0 cuda:1 cuda:2 cuda:3

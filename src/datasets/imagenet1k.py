updated scripts for pretraining with correct gradient accumulation

src/datasets/imagenet1k.py
@@ -33,7 +33,8 @@ def make_imagenet1k(
     training=True,
     copy_data=False,
     drop_last=True,
-    subset_file=None
+    subset_file=None,
+    shuffle=False
 ):
     dataset = ImageNet(
         root=root_path,
@@ -48,7 +49,8 @@ def make_imagenet1k(
     dist_sampler = torch.utils.data.distributed.DistributedSampler(
         dataset=dataset,
         num_replicas=world_size,
-        rank=rank)
+        rank=rank,
+        shuffle=shuffle)
     data_loader = torch.utils.data.DataLoader(
         dataset,
         collate_fn=collator,
@@ -57,7 +59,8 @@ def make_imagenet1k(
         drop_last=drop_last,
         pin_memory=pin_mem,
         num_workers=num_workers,
-        persistent_workers=False)
+        persistent_workers=False,
+        )
     logger.info('ImageNet unsupervised data loader created')
 
     return dataset, data_loader, dist_sampler
@@ -76,6 +79,7 @@ def make_imagenet1k_supervised(
     copy_data=False,
     drop_last=True,
     subset_file=None,
+    shuffle=False
 ):
     """
     Creates supervised ImageNet dataloader for KNN evaluation
@@ -87,7 +91,7 @@ def make_imagenet1k_supervised(
         transform=transform,
         train=training,
         copy_data=copy_data,
-        index_targets=True)  # Always index targets for supervised loading
+        index_targets=True,)  # Always index targets for supervised loading
     
     if subset_file is not None:
         dataset = ImageNetSubset(dataset, subset_file)
@@ -98,7 +102,8 @@ def make_imagenet1k_supervised(
     dist_sampler = torch.utils.data.distributed.DistributedSampler(
         dataset=dataset,
         num_replicas=world_size,
-        rank=rank)
+        rank=rank,
+        shuffle=shuffle)
 
     # def my_collator(batch):
     #     collated_batch = torch.utils.data.default_collate(batch)
@@ -114,7 +119,8 @@ def make_imagenet1k_supervised(
         collate_fn=torch.utils.data.default_collate,
         pin_memory=pin_mem,
         num_workers=num_workers,
-        persistent_workers=False)
+        persistent_workers=False,
+        )
     
     logger.info('ImageNet supervised data loader created')
     # logger.critical('%s %s %s' % (str(dataset), 

src/train.py
@@ -288,7 +288,8 @@ def main(args, resume_preempt=False):
             root_path=root_path,
             image_folder=image_folder,
             copy_data=copy_data,
-            drop_last=True)
+            drop_last=True,
+            shuffle=True)
     
     _, train_loader, _ = make_imagenet1k_supervised(
         transform=transform,
@@ -301,7 +302,8 @@ def main(args, resume_preempt=False):
         rank=rank,
         root_path=root_path,
         image_folder=image_folder,
-        copy_data=copy_data,)
+        copy_data=copy_data,
+        shuffle=True)
     
     _, test_loader, _ = make_imagenet1k_supervised(
         transform=transform,
@@ -314,7 +316,8 @@ def main(args, resume_preempt=False):
         rank=rank,
         root_path=root_path,
         image_folder=image_folder,
-        copy_data=copy_data,)
+        copy_data=copy_data,
+        shuffle=True)
     ipe = len(unsupervised_loader) # iterations per epoch
 
     # -- init optimizer and scheduler
@@ -396,7 +399,7 @@ def main(args, resume_preempt=False):
             log_freq = ipe // logging_frequency # report every X := `logging_frequency` intermediate steps
         # -- update distributed-data-loader epoch
         unsupervised_sampler.set_epoch(epoch)
-        optimizer.zero_grad() # for safety
+        # optimizer.zero_grad() # for safety
 
         loss_meter = AverageMeter()
         maskA_meter = AverageMeter()
@@ -468,17 +471,17 @@ def main(args, resume_preempt=False):
                 #  Step 2. Backward & step
                 if use_bfloat16:
                     scaler.scale(loss).backward()
-                    if (itr+1) % accumulate_grads_every == 0 or (itr + 1 == len(unsupervised_loader)): # accumulate_grads_every = 1 cancels grad accumulation
+                    if (itr+1) % accumulate_grads_every == 0: # or (itr + 1 == len(unsupervised_loader)): # accumulate_grads_every = 1 cancels grad accumulation
                         scaler.step(optimizer)
                         scaler.update()
-                        grad_stats = grad_logger(encoder.named_parameters())
+                        # grad_stats = grad_logger(encoder.named_parameters())
                         optimizer.zero_grad()
                 else:
                     loss.backward()
-                    if (itr+1) % accumulate_grads_every == 0 or (itr + 1 == len(unsupervised_loader)):
+                    if (itr+1) % accumulate_grads_every == 0: # or (itr + 1 == len(unsupervised_loader)):
                         optimizer.step()
-                        grad_stats = grad_logger(encoder.named_parameters())
                         optimizer.zero_grad()
+                grad_stats = grad_logger(encoder.named_parameters())
 
                 # Step 3. momentum update of target encoder
                 with torch.no_grad():